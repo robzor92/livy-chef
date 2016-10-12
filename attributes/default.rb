f39d0a345835952482b07e6b7f85ff73fe73c566
ErmiasG <ermiasg@kth.se>
2016-10-12 17:46:19 +0200

    made livy.spark.master=yarn and removed version num for spark and hadoop

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,12 +5,12 @@ default.livy.version                 = "0.3.0-SNAPSHOT"
 default.livy.url                     = "#{node.download_url}/livy-server-#{node.livy.version}.zip"
 default.livy.port                    = "8998"
 default.livy.dir                     = "/srv"
-default.livy.base_dir                =  node.livy.dir + "/livy-server-" + node.livy.version
-default.livy.home                    =  node.livy.dir + "/livy-server" 
+default.livy.base_dir                = node.livy.dir + "/livy-server" 
+default.livy.home                    = node.livy.dir + "/livy-server-" + node.livy.version  
 default.livy.keystore                = "#{node.kagent.base_dir}/node_server_keystore.jks"
 default.livy.keystore_password       = node.hopsworks.master.password
 
 default.livy.pid_file                = "/tmp/livy.pid"
-default.livy.log                     = "#{node.livy.home}/livy.log"
+default.livy.log                     = "#{node.livy.base_dir}/livy.log"
 
 default.livy.systemd                 = "true"
