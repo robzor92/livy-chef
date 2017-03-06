b01c26c2e6707910eea09384dfd3fa5b900cf331
Jim Dowling <jdowling@sics.se>
2017-03-06 08:07:29 +0100

    Adding global 'node.install.dir' attr

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,14 @@
+include_attribute "kagent"
+
 default.livy.user                    = node.hadoop_spark.user
 default.livy.group                   = node.hadoop_spark.group
 
 default.livy.version                 = "0.3.0-SNAPSHOT"
 default.livy.url                     = "#{node.download_url}/livy-server-#{node.livy.version}.zip"
 default.livy.port                    = "8998"
-default.livy.dir                     = "/srv"
-default.livy.home                    =  node.livy.dir + "/livy-server-" + node.livy.version
-default.livy.base_dir                =  node.livy.dir + "/livy-server" 
+default.livy.dir                     = node.install.dir.empty? ? node.install.dir : "/srv"
+default.livy.home                    = node.livy.dir + "/livy-server-" + node.livy.version
+default.livy.base_dir                = node.livy.dir + "/livy-server" 
 default.livy.keystore                = "#{node.kagent.base_dir}/node_server_keystore.jks"
 default.livy.keystore_password       = node.hopsworks.master.password
 
